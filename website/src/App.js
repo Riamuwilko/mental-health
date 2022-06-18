55db5e38e87f94db87a67f8eccd498278eb31d22
Deltafox137 <dlin4532@gmail.com>
2022-06-18 17:06:54 -0400

    init

--- website/src/App.js
+++ website/src/App.js
@@ -2,19 +2,19 @@ import React from 'react';
 import MainNavigation from './components/MainNavigation';
 import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
+import Mission from './pages/Mission';
 import Learn from './pages/Learn';
 import Resources from './pages/Resources';
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: "http://localhost:3000/" }}>
+    <div className="App" >
       <Router>
         <MainNavigation/>
         <main>
           <Routes>
           <Route path="/" element={<Home />} exact/>
-          <Route path="/about" element={<About />} exact/>
+          <Route path="/mission" element={<Mission />} exact/>
           <Route path="/learn" element={<Learn />} exact/>
           <Route path="/resources" element={<Resources />} exact/>
           <Route path="*" element={ <Navigate to="/" /> } />
--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -1,15 +1,58 @@
 import React from 'react';
-import './pageindex.css';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import { CardContent, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import {useNavigate} from 'react-router-dom';
+
 
 const Home = () => {
+    const navigate = useNavigate();
     return (
-        <div class="center-screen">
-            <p>
-                "I hope you realize that every day
-                is a fresh start for you. That every
-                sunrise is a new chapter in your
-                life waiting to be written."
-            </p>
+        <div>
+            <Grid
+                container
+                justify="center"
+                alignItems="center"
+                direction="column"
+                style={{minHeight: "95vh",
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center"
+                        }}
+            >
+                <Grid 
+                    container 
+                    direction="column"
+                    alignItems="center"
+                    style={{
+                        maxWidth: 1475
+                    }}
+                >
+                    <Card sx={{ maxWidth: 1475, maxHeight:595 }}>
+                        <CardMedia
+                        component="img"
+                        image="/image/mentalhealthimage.png"
+                        alt="sun"
+                        />
+                    </Card>
+                    <br></br>
+                    <Card elevation={0}>
+                        <CardContent style={{background: "#e1d4b7" }}>
+                            <Typography fontFamily="Brush Script MT, Brush Script Std, cursive" fontSize={36} style={{textAlign: "center"}}>
+                                "I hope you realize that every day
+                                is a fresh start for you. That every
+                                sunrise is a new chapter in your
+                                life waiting to be written." -Juansen Dizon
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                    <Button onClick={() => navigate("/learn")} variant="contained" size="large" style={{ background: "#68a9b0", maxWidth: 300}}>
+                        your journey starts here
+                    </Button>
+                </Grid>
+            </Grid>
         </div>
     );
 };
