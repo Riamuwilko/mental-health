2287ad030a975feec40aa3135acfdda05865bb2e
Deltafox137 <dlin4532@gmail.com>
2022-06-18 18:11:13 -0400

    init

--- website/src/pages/Mission.js
+++ website/src/pages/Mission.js
@@ -1,31 +1,81 @@
 import React from 'react';
-import {Typography} from '@mui/material';
+import {CardContent, Typography} from '@mui/material';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
 
 
 const Mission = () => {
     return (
-        <div style={{position: "relative", top: "100px"}}>
+        <div style={{position: "relative", top: "100px", paddingBottom: "200px"}}>
            <Typography fontFamily="Aeonik" fontSize={20} style={{textAlign: "center"}}>
                 Our Mission
             </Typography>
             <Typography fontFamily="Aeonik" fontSize={40} style={{textAlign: "center"}}>
-                We believe in a more sustainable and conscientious way of living.
+                We are here for you.
             </Typography>
-            <Typography fontFamily="Aeonik" fontSize={20} style={{marginLeft: "500px", marginRight: "500px", textAlign: "center"}}>
-                We believe our team, our residents,
-                and our partners must reflect the diversity of the communities we serve.
-                We're contributing to build a future where everyone has the support
-                they need to make time for the people and things that matter most.
+            <Typography fontFamily="Aeonik" fontSize={20} style={{textAlign: "center"}}>
+                We understand that you are living through an unprecedented period of time.<br></br>
+                There are a lot of unknowns in the world right now.<br></br>
+                But we are here to help.
             </Typography>
-            <Card sx={{ maxWidth: 1475, maxHeight:595 }}>
-                <CardMedia
-                    component="img"
-                    image="/image/mentalhealthimage.png"
-                    alt="sun"
-                />
-            </Card>
+            <br></br>
+            <Grid
+                container
+                justify="center"
+                alignItems="center"
+                columnSpacing={10}
+                rowSpacing={5}
+                style={{minHeight: "20vh",
+                        display: "flex",
+                        justifyContent: "center"
+                    }}
+            >
+                <Grid item xs={12} style={{display: "flex", justifyContent: "center" }}>
+                    <Card elevation={0} sx={{width: 800, height:400 }}>
+                        <CardMedia
+                            component="img"
+                            image="/image/dilemna.png"
+                            alt="sun"
+                        />
+                    </Card>
+                </Grid>
+                <Grid item xs={12} style={{display: "flex", justifyContent: "center" }}>
+                    <Typography fontFamily="Aeonik" fontSize={30} style={{textAlign: "center"}}>
+                        Meet the team
+                    </Typography>
+                </Grid>
+                <Grid item xs={6} style={{display: "flex", justifyContent: "flex-end" }}>
+                    <Card sx={{ width: 350, height: 400 }}>
+                        <CardMedia
+                            component="img"
+                            image="/image/sun.png"
+                            alt="sun"
+                            sx={{ width: 350, height: 300 }}
+                        />
+                        <CardContent>
+                            <Typography>
+                                dsfdfasd
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={6} style={{display: "flex", justifyContent: "flex-start" }}>
+                    <Card sx={{ width: 350, height: 400 }}>
+                        <CardMedia
+                            component="img"
+                            image="/image/sun.png"
+                            alt="sun"
+                            sx={{ width: 350, height: 300 }}
+                        />
+                        <CardContent>
+                            <Typography>
+                                dsfdfasd
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </div>
     );
 };
